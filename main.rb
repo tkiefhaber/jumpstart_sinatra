ee5dbe1a969b7573df8906a49a280769201d4787
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2013-03-14 22:12:34 -0400

    email contact form

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'sinatra/flash'
+require 'sinatra-flash'
 require 'slim'
 require 'sass'
+require 'pony'
 require './song'
 
 configure do
@@ -13,10 +14,18 @@ end
 
 configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  set :email_address => 'smtp.gmail.com',
+      :email_user_name => 'tkiefhab',
+      :email_password => 't0m@kiEf',
+      :email_domain => 'localhost.localdomain'
 end
 
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
+  set :email_address => 'smtp.sendgrid.net',
+      :email_user_name => ENV['SENDGRID_USERNAME'],
+      :email_password => ENV['SENDGRID_PASSWORD'],
+      :email_domain => 'heroku.com'
 end
 
 before do
@@ -52,6 +61,12 @@ end
 
 get '/contact' do
   slim :contact
+end
+
+post '/contact' do
+  send_message
+  flash[:notice] = "Thanks for your message, we'll be in touch"
+  redirect to('/')
 end
 
 get '/songs' do
@@ -116,6 +131,26 @@ helpers do
 
   def set_title
     @title ||= "Songs by Sinatra"
+  end
+
+  def send_message
+    Pony.mail(
+      :from => params[:name] + "<" + params[:email] + ">",
+      :to => 'tkiefhab@gmail.com',
+      :subject => params[:name] + "has reached out to you",
+      :body => params[:message],
+      :port => '587',
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.gmail.com',
+        :port => '587',
+        :enable_starttls_auto => true,
+        :username => 'tkiefhab',
+        :password => 't0m@kiEf',
+        :authentication => 'plain',
+        :domain => 'localhost.localdomain'
+      }
+    )
   end
 end
 
